9108a39e7acc63eec7b778332e16627f5101ea5e
Bagus Panji <47809071+Baguspanji@users.noreply.github.com>
2026-01-16 03:03:48 +0700

    Add redirect URL method to resource pages for consistent navigation

--- app/Filament/Resources/AffiliateStores/Pages/CreateAffiliateStore.php
+++ app/Filament/Resources/AffiliateStores/Pages/CreateAffiliateStore.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAffiliateStore extends CreateRecord
 {
     protected static string $resource = AffiliateStoreResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
